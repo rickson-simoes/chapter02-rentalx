bc44dc46d0a3a3fdd7a3a35e3d81a8a6b1ed5593
Rickson <rickson.simoes@hotmail.com>
2021-06-03 17:36:12 -0300

    :wrench: :hammer: Configuracoes para executar projeto e teste primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
 import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.status(201).json({ message: "Hello world" });
+});
+
+app.listen(3333, () => {
+  console.log("🚀 Servidor Rodando!! 👨‍💻");
+});
